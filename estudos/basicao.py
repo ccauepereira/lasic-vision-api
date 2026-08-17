3c4ce4c305b81138bec403cc272784073634e455
ccauepereira <cauecpereira06@gmail.com>
2026-08-17 11:42:48 -0300

    estudos: conhecendo

---
+++ estudos/basicao.py
@@ -0,0 +1,25 @@
+nome = "Cauê Pereira"
+semestre_ccomp = 2
+semestre_tecinfor = 3
+bolsa_lasic = 700.0
+estudando_python = True
+
+print(nome)
+print(semestre_ccomp)
+print(semestre_tecinfor)
+print(bolsa_lasic)
+print(estudando_python)
+
+if bolsa_lasic >= 700:
+   print("Bolsa massa")
+else:
+   print("Bolsa nada massa")
+
+   nivel_python = 1
+
+if nivel_python >= 8:  
+    print("Python avançado")
+elif nivel_python >= 5: 
+    print("Python basico")
+else: 
+    print("Python ruim")
