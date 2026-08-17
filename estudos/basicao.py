e951f41b958261741f76762583095e1401bad64e
ccauepereira <cauecpereira06@gmail.com>
2026-08-17 17:13:04 -0300

    estudando: fix python

--- estudos/basicao.py
+++ estudos/basicao.py
@@ -1,3 +1,6 @@
+from .nivel_python import nivel_python
+
+
 nome = "Cauê Pereira"
 semestre_ccomp = 2
 semestre_tecinfor = 3
@@ -14,8 +17,6 @@ if bolsa_lasic >= 700:
    print("Bolsa massa")
 else:
    print("Bolsa nada massa")
-
-   nivel_python = 1
 
 if nivel_python >= 8:  
     print("Python avançado")
